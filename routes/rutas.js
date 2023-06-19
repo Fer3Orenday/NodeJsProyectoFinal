8001f76af5bfc6bcb6d38c2aa666bda6831e9d00
Fer3Orenday <fer.orenday3@gmail.com>
2023-06-18 20:49:47 -0600

    servicios listos

--- routes/rutas.js
+++ routes/rutas.js
@@ -5,6 +5,20 @@ const connection = require("../conexion");
 const { body, param, validationResult } = require("express-validator");
 var router = express.Router();
 
+const admin = require('firebase-admin');
+
+const serviceAccount = require('../credenciales.json'); // Ruta a tu archivo de credenciales descargado desde Firebase
+
+// Inicializar la conexión a Firebase
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://tu-proyecto.firebaseio.com'
+});
+
+// Obtener una referencia a la colección deseada
+const db = admin.firestore();
+const collectionRef = db.collection('citas');
+
 // esta ruta GET se utiliza para obtener todos los usuarios de 
 // la base de datos y enviarlos como respuesta en formato JSON al cliente que realiza la solicitud.
 router.get("/user", [], (req, res) => {
@@ -13,6 +27,62 @@ router.get("/user", [], (req, res) => {
   });
 });
 
+router.post("/userQr", [], (req, res) => {
+  let body = req.body;
+console.log(body.fullName);
+  collectionRef.where('data.fullName', '==', body.fullName)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+        console.log('No hay nada')
+        res.json({
+          success: true,
+          result: doc.data(),
+        })
+      });
+    })
+    .catch((error) => {
+      res.json({
+        success: false,
+        result: error,
+      })
+    });
+
+});
+
+router.post("/cancelar", [], (req, res) => {
+  let body = req.body;
+
+  collectionRef.where('data.fullName', '==', body.fullName)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+        const documentRef = collectionRef.doc(doc.id);
+        documentRef.delete()
+          .then(() => {
+            res.json({
+              success: true,
+            })
+          })
+          .catch((error) => {
+            res.json({
+              success: false,
+              msg: error
+            })
+          });
+
+      });
+    })
+    .catch((error) => {
+      res.json({
+        success: false,
+        result: error,
+      })
+    });
+
+});
 
 router.post(
   "/user",
@@ -23,9 +93,10 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.json({ success: false, err: JSON.stringify(errors) });
-      return;
+      return
     } else {
       let body = req.body;
+      console.log(body);
       // configura el  correo electrónico utilizando Gmail como servicio de correo saliente. 
       // Se proporcionan las credenciales de autenticación,
       //  donde user es la dirección de correo electrónico del remitente y pass es la contraseña correspondiente.
@@ -40,11 +111,11 @@ router.post(
       //En esta parte se manda el contenido del correo electronico
       const mailOptions = {
         from: 'Maria Fernanda Hermosillo Orenday',
-        to: body.contact,
+        to: body.email,
         //body.asunto es para poner en el mensaje el asunto que va a mandar el usuario
-        subject: `Recibimos tu comentario acerca de: ${body.asunto}`,
+        subject: `Reservacion confirmada a nombre de: ${body.fullName}`,
         //body.asunto es para poner en el mensaje la descripcion que va a mandar el usuario
-        text: `Tu comentario se atenderá lo antes posible. Tu comentario fue: ${body.descripcion}`
+        text: `Tu reservacion ha sido realizada con exito en  ${body.casaReservada.title}, con fecha del ${body.arriveDate} al ${body.lastDate}, con un total de ${body.huespedes} huespedes`
       };
 
 
